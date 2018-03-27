d17d48fa584cb00fa619b912c0f18a11d1796739
Apara V <ssapar@gmail.com>
2018-03-27 16:06:18 -0600

    Add some test data

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-from aco.colonization import ant_colony
+import os
 import math
+from aco.colonization import ant_colony
 
-test_nodes = {0: (12, 4), 1: (3, 9), 2: (3, 8), 3: (14, 11), 4: (8, 11),
-	5: (15, 6), 6: (6, 15), 7: (15, 9), 8: (12, 10), 9: (10, 7)}
+
+data_path = "data"
+
+def read_file(fname):
+
+	found_cities = False
+	graph = dict()
+
+	with open(fname) as f:
+		for line in f:
+			if found_cities:
+				line = line.split()
+				graph[line[0]] = (float(line[1]), float(line[2]))
+
+			if line == 'NODE_COORD_SECTION\n':
+				found_cities = True
+
+	return graph
 
 def distance(start, end):
 	x_distance = abs(start[0] - end[0])
 	y_distance = abs(start[1] - end[1])
-	
+		
 	return math.sqrt(pow(x_distance, 2) + pow(y_distance, 2))
 
-colony = ant_colony(test_nodes, distance, start=1)
+if __name__ == "__main__":
 
-path, dist = colony.run()
-print(path)
-print(dist)
+	fname = os.path.join(data_path, "djibouti.tsp")
+	nodes = read_file(fname)
+
+	colony = ant_colony(nodes, distance, start='1', iterations=200)
+
+	path, dist = colony.run()
+	print(path)
+	print(dist)
